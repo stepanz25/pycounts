20c55cba3373b17821b954f1cea82d59de2ae567
stepanz25 <112679383+stepanz25@users.noreply.github.com>
2023-01-09 20:46:42 -0800

    updated the docstring

--- src/pycounts/pycounts.py
+++ src/pycounts/pycounts.py
@@ -16,17 +16,21 @@ def clean_text(text):
     return text
     
 def count_words(input_file):
-    """ Count words in a text file.
+    """Count words in a text file.
+
     Words are made lowercase and punctuation is removed 
     before counting.
+
     Parameters
     ----------
     input_file : str
         Path to text file.
+
     Returns
     -------
     collections.Counter
         dict-like object where keys are words and values are counts.
+
     Examples
     --------
     >>> count_words("text.txt")
